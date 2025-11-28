adc0f3a3102653d2a2072696a18245d6b055ce2c
Bartłomiej Kuras <hash.deb@gmail.com>
2025-11-28 20:27:41 +0100

    test: users context tests

--- gq-server/src/context/users.rs
+++ gq-server/src/context/users.rs
@@ -50,47 +50,41 @@ impl fmt::Display for UserId {
 }
 
 /// User queryable data
-#[derive(Debug, Clone, GraphQLObject)]
+#[derive(Debug, Clone, PartialEq, GraphQLObject)]
 pub struct User {
     /// How user is visible to others.
     pub nickname: String,
 }
-
-/// User data
-pub struct UserData {
-    /// How user is visible to others.
-    pub nickname: String,
-}
-
+//
 // Users storage.
 #[derive(Derivative)]
 #[derivative(Default(new = "true"))]
 pub struct Users {
     /// Users map
-    users: RwLock<HashMap<Uuid, UserData>>,
+    users: RwLock<HashMap<Uuid, User>>,
 }
 
 impl Users {
     /// Returns single user by their id
     pub async fn user(&self, user_id: UserId) -> Option<User> {
-        self.users.read().await.get(&user_id.0).map(|data| User {
-            nickname: data.nickname.clone(),
-        })
+        self.users.read().await.get(&user_id.0).cloned()
     }
 
     /// Returns users hashmap locked for read.
-    pub async fn users(&self) -> RwLockReadGuard<'_, HashMap<Uuid, UserData>> {
+    pub async fn users(&self) -> RwLockReadGuard<'_, HashMap<Uuid, User>> {
         self.users.read().await
     }
 
     /// Create a new user returning created user id.
-    pub async fn create(&self, nickname: String) -> UserId {
+    pub async fn create(&self, nickname: impl Into<String>) -> UserId {
+        let nickname = nickname.into();
+
         let mut users = self.users.write().await;
         loop {
             let user_id = Uuid::new_v4();
             let entry = users.entry(user_id);
             if let hash_map::Entry::Vacant(entry) = entry {
-                entry.insert(UserData { nickname });
+                entry.insert(User { nickname });
                 return UserId(user_id);
             }
         }
@@ -102,5 +96,77 @@ impl juniper::Context for Users {}
 impl FromContext<Context> for Users {
     fn from(context: &Context) -> &Self {
         context.users()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn users_empty_initially() {
+        let users = Users::default();
+
+        assert!(users.users().await.is_empty())
+    }
+
+    #[tokio::test]
+    async fn creating_users() {
+        let users = Users::default();
+
+        let user1 = users.create("user1").await;
+        {
+            let users = users.users().await;
+            assert_eq!(users.len(), 1);
+            assert_eq!(
+                &User {
+                    nickname: "user1".to_owned()
+                },
+                users.get(&user1.0).unwrap()
+            );
+        }
+
+        let user2 = users.create("user2").await;
+        {
+            let users = users.users().await;
+            assert_eq!(users.len(), 2);
+            assert_eq!(
+                &User {
+                    nickname: "user1".to_owned()
+                },
+                users.get(&user1.0).unwrap()
+            );
+            assert_eq!(
+                &User {
+                    nickname: "user2".to_owned()
+                },
+                users.get(&user2.0).unwrap()
+            );
+        }
+
+        // Username *can* collide
+        let user3 = users.create("user1").await;
+        {
+            let users = users.users().await;
+            assert_eq!(users.len(), 3);
+            assert_eq!(
+                &User {
+                    nickname: "user1".to_owned()
+                },
+                users.get(&user1.0).unwrap()
+            );
+            assert_eq!(
+                &User {
+                    nickname: "user2".to_owned()
+                },
+                users.get(&user2.0).unwrap()
+            );
+            assert_eq!(
+                &User {
+                    nickname: "user1".to_owned()
+                },
+                users.get(&user3.0).unwrap()
+            );
+        }
     }
 }
